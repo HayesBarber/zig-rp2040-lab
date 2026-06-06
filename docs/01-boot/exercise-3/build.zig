4b37e2022610d91caa308a1cb937d6d3887dc5af
Hayes Barber <barberhayes@gmail.com>
2026-06-06 17:38:13 -0400

    rename to blinky.zig

--- docs/01-boot/exercise-3/build.zig
+++ docs/01-boot/exercise-3/build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "blinky",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("main.zig"),
+            .root_source_file = b.path("blinky.zig"),
             .target = target,
             .optimize = optimize,
         }),
