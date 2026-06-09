7125f05a2034105d1530e48892866d20a76f12ea
Hayes Barber <barberhayes@gmail.com>
2026-06-09 18:44:51 -0400

    call sleep_ms from pico-sdk

--- docs/02-sdk-interop/exercise-4/blinky.zig
+++ docs/02-sdk-interop/exercise-4/blinky.zig
@@ -1,18 +1,34 @@
-extern fn gpio_init(gpio: u32) void;
-extern fn gpio_set_dir(gpio: u32, out: bool) void;
-extern fn gpio_put(gpio: u32, value: bool) void;
 extern fn sleep_ms(ms: u32) void;
 
-const LED_PIN = 25;
-
 export fn main() noreturn {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, true);
+    const PUT32 = struct {
+        fn write(addr: u32, value: u32) void {
+            @as(*volatile u32, @ptrFromInt(addr)).* = value;
+        }
+    };
+
+    const GET32 = struct {
+        fn read(addr: u32) u32 {
+            return @as(*volatile u32, @ptrFromInt(addr)).*;
+        }
+    };
+
+    // IO BANK
+    PUT32.write(0x4000f000, (1 << 5));
+
+    // Reset done?
+    while ((GET32.read(0x4000c008) & (1 << 5)) == 0) {}
+
+    // IO PAD = FUNC 5 (GPIO)
+    PUT32.write(0x400140cc, 0x05);
+
+    // GPIO_OE
+    PUT32.write(0xd0000020, (1 << 25));
 
     while (true) {
-        gpio_put(LED_PIN, true);
-        sleep_ms(1000);
-        gpio_put(LED_PIN, false);
+        PUT32.write(0xd000001c, (1 << 25));
+        sleep_ms(100);
+        PUT32.write(0xd000001c, (1 << 25));
         sleep_ms(1000);
     }
 }
