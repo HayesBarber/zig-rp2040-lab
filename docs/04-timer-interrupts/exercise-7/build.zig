fde56e2e9c64714a9cf2a7ef7f9dfaca57ca04f8
Hayes Barber <barberhayes@gmail.com>
2026-06-12 21:20:27 -0400

    init exercise 7 code

---
+++ docs/04-timer-interrupts/exercise-7/build.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize: std.builtin.OptimizeMode = .ReleaseSmall;
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .thumb,
+        .os_tag = .freestanding,
+        .abi = .eabi,
+        .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_m0plus },
+    });
+
+    const main_object = b.addObject(.{
+        .name = "main_o",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    const install_object = b.addInstallFile(main_object.getEmittedBin(), "main.o");
+    b.getInstallStep().dependOn(&install_object.step);
+}
