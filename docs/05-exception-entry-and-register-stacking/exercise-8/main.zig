c2bac18aea61d1375df4d4efb528b795ad4a5f0b
Hayes Barber <barberhayes@gmail.com>
2026-06-15 22:00:42 -0400

    print stack pointer

--- docs/05-exception-entry-and-register-stacking/exercise-8/main.zig
+++ docs/05-exception-entry-and-register-stacking/exercise-8/main.zig
@@ -6,6 +6,25 @@ export fn isr_systick() void {
     TICK_COUNTER += 1;
     if (TICK_COUNTER == 1000) {
         TICK_COUNTER = 0;
+        var sp: u32 = undefined;
+
+        asm volatile ("mov %[out], sp"
+            : [out] "=r" (sp),
+        );
+
+        const frame = @as([*]const u32, @ptrFromInt(sp));
+
+        pico.printf(
+            "r0=%08x r1=%08x r2=%08x r3=%08x\n" ++ "r12=%08x lr=%08x pc=%08x xpsr=%08x\n",
+            frame[0],
+            frame[1],
+            frame[2],
+            frame[3],
+            frame[4],
+            frame[5],
+            frame[6],
+            frame[7],
+        );
     }
 }
 
