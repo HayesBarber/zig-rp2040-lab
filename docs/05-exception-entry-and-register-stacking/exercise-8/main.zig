cbfedb88f80e2fd54aca1932ce288bbac1d87636
Hayes Barber <barberhayes@gmail.com>
2026-06-15 22:19:06 -0400

    use naked isr func

--- docs/05-exception-entry-and-register-stacking/exercise-8/main.zig
+++ docs/05-exception-entry-and-register-stacking/exercise-8/main.zig
@@ -2,30 +2,30 @@ const pico = @import("pico.zig");
 
 var TICK_COUNTER: u32 = 0;
 
-export fn isr_systick() void {
+export fn isr_systick() callconv(.naked) void {
+    asm volatile (
+        \\ mrs r0, msp
+        \\ ldr r1, =isr_systick_impl
+        \\ bx r1
+    );
+}
+
+export fn isr_systick_impl(frame: [*]const u32) void {
     TICK_COUNTER += 1;
-    if (TICK_COUNTER == 1000) {
-        TICK_COUNTER = 0;
-        var sp: u32 = undefined;
+    if (TICK_COUNTER < 1000) return;
+    TICK_COUNTER = 0;
 
-        asm volatile ("mov %[out], sp"
-            : [out] "=r" (sp),
-        );
-
-        const frame = @as([*]const u32, @ptrFromInt(sp));
-
-        pico.printf(
-            "r0=%08x r1=%08x r2=%08x r3=%08x\n" ++ "r12=%08x lr=%08x pc=%08x xpsr=%08x\n",
-            frame[0],
-            frame[1],
-            frame[2],
-            frame[3],
-            frame[4],
-            frame[5],
-            frame[6],
-            frame[7],
-        );
-    }
+    pico.printf(
+        "r0=%08x r1=%08x r2=%08x r3=%08x " ++ "r12=%08x lr=%08x pc=%08x xpsr=%08x\n",
+        frame[0],
+        frame[1],
+        frame[2],
+        frame[3],
+        frame[4],
+        frame[5],
+        frame[6],
+        frame[7],
+    );
 }
 
 export fn main() noreturn {
