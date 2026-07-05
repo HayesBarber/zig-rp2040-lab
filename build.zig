5e29849f531d92566e01b1700b0fa113eaa716a5
Hayes Barber <barberhayes@gmail.com>
2026-07-05 18:30:00 -0400

    add build_boot2

--- build.zig
+++ build.zig
@@ -8,4 +8,27 @@ pub fn build(b: *std.Build) void {
         .abi = .eabi,
         .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_m0plus },
     });
+
+    _ = build_boot2(b, target, optimize);
+}
+
+fn build_boot2(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) std.Build.LazyPath {
+    const boot2_exe = b.addExecutable(.{
+        .name = "boot2-w25q080",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    boot2_exe.setLinkerScript(b.path("src/boot/stage2/shared/stage2.ld"));
+    boot2_exe.root_module.addAssemblyFile(b.path("src/boot/stage2/w25q080.S"));
+    boot2_exe.entry = .{ .symbol_name = "_stage2_boot" };
+
+    const boot2_objcopy = b.addObjCopy(boot2_exe.getEmittedBin(), .{
+        .basename = "w25q080.bin",
+        .format = .bin,
+    });
+
+    return boot2_objcopy.getOutput();
 }
