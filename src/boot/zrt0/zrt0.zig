27f0558bc1f45b30901e8bca01803272ba9eab7f
HayesBarber <barberhayes@gmail.com>
2026-07-06 22:44:13 -0400

    change vector table from array to struct

--- src/boot/zrt0/zrt0.zig
+++ src/boot/zrt0/zrt0.zig
@@ -2,23 +2,42 @@ const root = @import("root");
 
 extern const __stack_top: u8;
 
-pub const vector_table align(256) linksection(".vectors") = [_]usize{
-    @intFromPtr(&__stack_top), // 0: Initial Stack Pointer
-    @intFromPtr(&_start), // 1: Reset
-    0, // 2: NMI
-    0, // 3: HardFault
-    0, // 4: MemManage
-    0, // 5: BusFault
-    0, // 6: UsageFault
-    0, // 7: Reserved
-    0, // 8: Reserved
-    0, // 9: Reserved
-    0, // 10: Reserved
-    0, // 11: SVCall
-    0, // 12: Reserved
-    0, // 13: Reserved
-    0, // 14: PendSV
-    0, // 15: SysTick
+pub const VectorTable = extern struct {
+    initial_sp: *const anyopaque,
+    reset: *const fn () callconv(.c) noreturn,
+    nmi: ?*const anyopaque,
+    hard_fault: ?*const anyopaque,
+    reserved0: ?*const anyopaque,
+    reserved1: ?*const anyopaque,
+    reserved2: ?*const anyopaque,
+    reserved3: ?*const anyopaque,
+    reserved4: ?*const anyopaque,
+    reserved5: ?*const anyopaque,
+    reserved6: ?*const anyopaque,
+    svcall: ?*const anyopaque,
+    reserved7: ?*const anyopaque,
+    reserved8: ?*const anyopaque,
+    pendsv: ?*const anyopaque,
+    systick: ?*const anyopaque,
+};
+
+pub const vector_table align(256) linksection(".vectors") = VectorTable{
+    .initial_sp = &__stack_top,
+    .reset = &_start,
+    .nmi = null,
+    .hard_fault = null,
+    .reserved0 = null,
+    .reserved1 = null,
+    .reserved2 = null,
+    .reserved3 = null,
+    .reserved4 = null,
+    .reserved5 = null,
+    .reserved6 = null,
+    .svcall = null,
+    .reserved7 = null,
+    .reserved8 = null,
+    .pendsv = null,
+    .systick = null,
 };
 
 export fn _start() callconv(.c) noreturn {
