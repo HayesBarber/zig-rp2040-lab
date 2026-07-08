b64b3930ce1c8349250d9d0a3b36260204d8f910
Hayes Barber <barberhayes@gmail.com>
2026-07-07 21:51:07 -0400

    rename functions

--- src/main.zig
+++ src/main.zig
@@ -7,17 +7,17 @@ comptime {
 pub const GPIO_OUT_XOR = 0xd000001c;
 pub const LED_PIN = 25;
 
-pub fn put32(addr: u32, value: u32) void {
+pub fn putAddr(addr: u32, value: u32) void {
     @as(*volatile u32, @ptrFromInt(addr)).* = value;
 }
 
-pub fn get32(addr: u32) u32 {
+pub fn getAddr(addr: u32) u32 {
     return @as(*volatile u32, @ptrFromInt(addr)).*;
 }
 
 pub fn toggleGPIO(comptime pin: u8) void {
     const mask = 1 << pin;
-    put32(GPIO_OUT_XOR, mask);
+    putAddr(GPIO_OUT_XOR, mask);
 }
 
 pub fn toggleLED() void {
@@ -26,13 +26,13 @@ pub fn toggleLED() void {
 
 pub fn ledInit() void {
     // IO BANK
-    put32(0x4000f000, (1 << 5));
+    putAddr(0x4000f000, (1 << 5));
     // Reset done?
-    while ((get32(0x4000c008) & (1 << 5)) == 0) {}
+    while ((getAddr(0x4000c008) & (1 << 5)) == 0) {}
     // IO PAD = FUNC 5 (GPIO)
-    put32(0x400140cc, 0x05);
+    putAddr(0x400140cc, 0x05);
     // GPIO_OE
-    put32(0xd0000020, (1 << 25));
+    putAddr(0xd0000020, (1 << 25));
 }
 
 pub fn main() noreturn {
