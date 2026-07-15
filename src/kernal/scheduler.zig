17b1962cc4974f8d6f97ad5ec961578d6180c736
Hayes Barber <barberhayes@gmail.com>
2026-07-14 21:00:15 -0400

    init pendSV

--- src/kernal/scheduler.zig
+++ src/kernal/scheduler.zig
@@ -4,9 +4,20 @@ const mmio = core.mmio;
 const SYSTICK_RELOAD_VALUE = 125000 - 1; // 1 ms for 125MHz
 const SYSTICK_ENABLE_BITMASK = 0x7;
 
+const PENDSVSET = 1 << 28;
+const PENDSV_PRI_LOWEST = 0b11 << 22;
+
 fn initSystick() void {
     mmio.systick.rvr = SYSTICK_RELOAD_VALUE;
     mmio.systick.csr = SYSTICK_ENABLE_BITMASK;
+}
+
+fn setPendSVPriority() void {
+    mmio.scb.shpr3 |= PENDSV_PRI_LOWEST;
+}
+
+inline fn setPendSVPending() void {
+    mmio.scb.icsr = PENDSVSET;
 }
 
 var TICK_COUNTER: u32 = 0;
@@ -16,10 +27,15 @@ pub fn sysTickISR() callconv(.c) void {
     if (TICK_COUNTER < 1000) return;
     TICK_COUNTER = 0;
 
+    setPendSVPending();
+}
+
+pub fn pendsvISR() callconv(.c) void {
     core.gpio.toggleLED();
     core.uart.print("hello UART!\n\r");
 }
 
 pub fn start() void {
+    setPendSVPriority();
     initSystick();
 }
