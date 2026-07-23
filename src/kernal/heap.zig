53425a2e955794ae8deacf8b7794fcf064914e04
HayesBarber <barberhayes@gmail.com>
2026-07-22 22:10:37 -0400

    add heap to kernel mod

---
+++ src/kernal/heap.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+extern const __heap_start: u8;
+extern const __heap_end: u8;
+
+var fba: std.heap.FixedBufferAllocator = undefined;
+
+pub fn init() void {
+    const start = @intFromPtr(&__heap_start);
+    const end = @intFromPtr(&__heap_end);
+    const buf: []u8 = @as([*]u8, @ptrCast(@constCast(&__heap_start)))[0 .. end - start];
+    fba = std.heap.FixedBufferAllocator.init(buf);
+}
+
+pub fn allocator() std.mem.Allocator {
+    return fba.allocator();
+}
