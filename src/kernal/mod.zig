3b01e63a90db869791b192ce8e08f12e7df87622
Hayes Barber <barberhayes@gmail.com>
2026-07-27 21:52:20 -0400

    define selected algo in main.zig

--- src/kernal/mod.zig
+++ src/kernal/mod.zig
@@ -1,6 +1,8 @@
 pub const heap = @import("heap.zig");
 pub const isr = @import("isr/mod.zig");
 pub const scheduler = @import("scheduler/mod.zig");
+const algo = @import("scheduler/algorithms/mod.zig");
+pub const SchedulingAlgorithms = algo.Algorithm;
 pub const task = @import("task.zig");
 pub const uart = @import("isr/uart.zig");
 
--- src/kernal/scheduler/algorithms/mod.zig
+++ src/kernal/scheduler/algorithms/mod.zig
@@ -1,7 +1,7 @@
-const Algorithm = enum { super_loop, round_robin };
-const selected = Algorithm.round_robin;
+const root = @import("root");
+pub const Algorithm = enum { super_loop, round_robin };
 
-pub const SchedulerImpl = switch (selected) {
+pub const SchedulerImpl = switch (root.SCHEDULER_ALGORITHM) {
     .round_robin => @import("round_robin.zig"),
     .super_loop => @import("super_loop.zig"),
 };
