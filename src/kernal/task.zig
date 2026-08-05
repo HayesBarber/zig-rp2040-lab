76eaa1373d3470ed7e4a094aaf6c5b991127f62b
Hayes Barber <barberhayes@gmail.com>
2026-08-04 20:20:07 -0400

    create entry type based on algorithm

--- src/kernal/task.zig
+++ src/kernal/task.zig
@@ -1,6 +1,13 @@
+const root = @import("root");
+
+pub const Entry = switch (root.SCHEDULER_ALGORITHM) {
+    .super_loop => *const fn () void,
+    .round_robin => *const fn () noreturn,
+};
+
 pub const TaskEntry = struct {
     name: []const u8,
-    entry: *const fn () noreturn,
+    entry: Entry,
 };
 
 pub const TaskGroup = struct {
