54b24b163f4f663e0502e53082eadda2ba2b9227
Hayes Barber <barberhayes@gmail.com>
2026-08-06 21:41:33 -0400

    making ready queue generic and adding tests

--- src/kernal/scheduler/algorithms/multilevel-feedback-queue/ready_queue.zig
+++ src/kernal/scheduler/algorithms/multilevel-feedback-queue/ready_queue.zig
@@ -1,40 +1,76 @@
-const task = @import("tcb.zig");
-const TCB = task.TCB;
-
-const ReadyQueue = @This();
-
-head: ?*TCB = null,
-tail: ?*TCB = null,
-len: usize = 0,
-
-pub fn enqueue(self: *ReadyQueue, tcb: *TCB) void {
-    if (self.tail) |t| {
-        t.next = tcb;
-        t = t.next;
-    } else {
-        self.head = tcb;
-        self.tail = tcb;
+pub fn ReadyQueue(comptime T: type) type {
+    if (!@hasField(T, "next") or @TypeOf(@field(@as(T, undefined), "next")) != ?*T) {
+        @compileError("ReadyQueue requires a type with a field \"next\" of type ?*T");
     }
 
-    self.len += 1;
-}
+    return struct {
+        const Self = @This();
+        head: ?*T = null,
+        tail: ?*T = null,
+        len: usize = 0,
 
-pub fn dequeue(self: *ReadyQueue) ?*TCB {
-    const res = self.head;
-    if (res == null) return res;
+        pub fn enqueue(self: *Self, tcb: *T) void {
+            if (self.tail != null) {
+                self.tail.?.next = tcb;
+                self.tail = self.tail.?.next;
+            } else {
+                self.head = tcb;
+                self.tail = tcb;
+            }
 
-    self.head = res.next;
-    res.next = null;
-    self.len -= 1;
-    return res;
+            self.len += 1;
+        }
+
+        pub fn dequeue(self: *Self) ?*T {
+            const res = self.head;
+            if (res == null) return res;
+
+            self.head = res.?.next;
+            res.?.next = null;
+            self.len -= 1;
+            return res;
+        }
+    };
 }
 
 const std = @import("std");
 const expect = std.testing.expect;
 
+const TestNode = struct {
+    const Self = @This();
+
+    next: ?*Self = null,
+};
+
 test "Create ReadyQueue" {
-    const queue: ReadyQueue = .{};
-    try expect(queue.len == 0);
-    try expect(queue.head == null);
-    try expect(queue.tail == null);
+    const q: ReadyQueue(TestNode) = .{};
+    try expect(q.len == 0);
+    try expect(q.head == null);
+    try expect(q.tail == null);
+}
+
+test "Enqueue" {
+    var q: ReadyQueue(TestNode) = .{};
+    var head: TestNode = .{};
+    q.enqueue(&head);
+    try expect(q.len == 1);
+    try expect(q.head == &head);
+    try expect(q.tail == &head);
+
+    var tail: TestNode = .{};
+    q.enqueue(&tail);
+    try expect(q.len == 2);
+    try expect(q.head == &head);
+    try expect(q.tail == &tail);
+}
+
+test "Dequeue" {
+    var q: ReadyQueue(TestNode) = .{};
+    var head: TestNode = .{};
+    q.enqueue(&head);
+    try expect(q.len == 1);
+
+    const popped = q.dequeue();
+    try expect(q.len == 0);
+    try expect(&head == popped);
 }
